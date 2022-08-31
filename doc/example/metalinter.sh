3e88a593dcc6110e6b2ee18227e154ef8fb377b5
laojianzi <laojianzi1994@gmail.com>
2022-08-31 20:55:50 +0800

    fix: Quote parameter to prevent glob expansion SH-2060

--- doc/example/metalinter.sh
+++ doc/example/metalinter.sh
@@ -11,8 +11,8 @@ fi
 UPDATE=$1
 
 if [ "${UPDATE}X" != "X" ]; then
-	${GOPATH}/bin/gometalinter.v1 --config ${PROJECT_PATH}/doc/example/metalinter.json ./... | tr -d [0-9] | sort > ${PROJECT_PATH}/doc/example/metalinter.txt	
+	${GOPATH}/bin/gometalinter.v1 --config ${PROJECT_PATH}/doc/example/metalinter.json ./... | tr -d '0-9' | sort > ${PROJECT_PATH}/doc/example/metalinter.txt
 else
-	cd ${PROJECT_PATH} && diff <(${GOPATH}/bin/gometalinter.v1 --config ${PROJECT_PATH}/doc/example/metalinter.json ./... | tr -d [0-9] | sort) <(cat ${PROJECT_PATH}/doc/example/metalinter.txt)
+	cd ${PROJECT_PATH} && diff <(${GOPATH}/bin/gometalinter.v1 --config ${PROJECT_PATH}/doc/example/metalinter.json ./... | tr -d '0-9' | sort) <(cat ${PROJECT_PATH}/doc/example/metalinter.txt)
 fi
 
